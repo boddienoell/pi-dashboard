ed3a2d2d1992eaa33a3c3cbd256b58d050823466
Will Haley <willhaley@boddienoell.com>
2016-02-11 14:48:52 -0500

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dashboard', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ui.router', 'ngMaterial', 'toastr', 'ds.clock']);
+
+})();
