cd2dcbbd78e5bd3978bdbfa41d78a5d56f89afa5
Will Haley <willhaley@boddienoell.com>
2016-02-19 14:38:39 -0500

    updates for better news layouts and adds cafeteria info

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,9 +9,23 @@
   function MainController($interval) {
 
     var vm = this;
+    vm.slide = 1;
 
     $interval(function(){
-      document.getElementById('forecast_embed').contentWindow.location.reload();
-    }, 3600);
+      if ( vm.slide == 4 ){
+        vm.slide = 0;
+      } else {
+        vm.slide++;
+      }
+
+    },20000);
+
+    $interval(function(){
+      var url = document.getElementById('forecast_embed').src;
+      alert('asdf');
+      document.getElementById('forecast_embed').src = '';
+      document.getElementById('forecast_embed').src = url;
+
+    }, 600000);
   }
 })();
